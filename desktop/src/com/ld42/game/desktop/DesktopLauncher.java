a4e24b6c86e7c4544ed1e7977f49b96911304380
Александр Гоцулов <agotsulov@gmail.com>
2018-08-07 17:22:56 +0600

    first commit

---
+++ desktop/src/com/ld42/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.ld42.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.ld42.game.LD42;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new LD42(), config);
+	}
+}
